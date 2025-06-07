f94ebebe19b4b0a93ed2a1b9e98db230de0d8f13
Bhavyansh Tomar <105980945+thebhavyansh@users.noreply.github.com>
2025-06-08 03:16:06 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Devfolio | Portfolio Website using Next.js, Tailwind CSS, and Framer Motion",
-  description: "Devfolio is a portfolio website for developers to showcase their projects and skills.",
+  title: "Bhavyansh",
+  description: "My portfolio website",
 };
 
 export default function RootLayout({
