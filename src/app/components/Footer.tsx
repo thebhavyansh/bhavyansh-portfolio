f4b254466f9483f7ee88659828c8f53bc1807711
Bhavyansh Tomar <105980945+thebhavyansh@users.noreply.github.com>
2025-06-08 03:11:53 +0530

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 export default function Footer() {
   return (
